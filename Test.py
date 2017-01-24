bba10650513349ecf78d4e0b2186f59faa19d58d
KseniyaSol <gothica-k@mail.ru>
2017-01-24 14:15:11 +0400

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,15 +1,6 @@
-import http.client
-site = input("Введите сайт")
-conn = http.client.HTTPConnection(site)
-conn.request("GET", "/sandbox")
-r1 = conn.getresponse()
-print(r1.status)
+import requests
+r = requests.get('https://api.github.com/events')
+r.status_code
 
-data1 = r1.read()
-conn.request("GET", "/companies/")
-r2 = conn.getresponse()
-print(r2.status)
-
-data2 = r2.read()
-conn.close()
-input('Для продолжения нажмите [Enter] . . .')
+https://rtfm.co.ua/python-modul-requests/
+https://github.com/KseniyaSol/Test/tree/master
