355cfb313b93a73f44fd5770d8203cb1c5dc94f8
KseniyaSol <gothica-k@mail.ru>
2017-01-26 15:04:36 +0400

    Add files via upload

--- Browser.py
+++ Browser.py
@@ -1,9 +1,27 @@
 import requests
-def inp():
-    site = input('Enter site: ')
-    return site
-site = inp()
-rect = requests.get(site)
-print(rect.status_code)
 
-#'http://httpbin.org/status/415' - example of input data
+
+def inp(method):
+    if method == 'input':
+        ex = ['http://ex.org/']
+        corr = False
+        while (corr == False):
+            print('Example of input data: '+ex[0])
+            site = input('Enter site: ')         # 'http://httpbin.org/status/415' - example of input data
+            if site.find('http://') > -1:
+                corr = True
+                return site
+            else:
+                print('Input isnt correct!')
+    elif method == 'headers':
+        headers = rect.headers
+        return headers
+    elif method == 'code':
+        code = rect.status_code
+        return code
+site = inp('input')
+rect = requests.get(site)
+headers = inp('headers')
+print('Headers of '+str(site)+' is '+str(headers))
+code = inp('code')
+print(str(code)+' '+rect.reason)
